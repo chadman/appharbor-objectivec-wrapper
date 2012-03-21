8b5c0b8caccaecfc3aad1789a22501ca083fd779
Chad Meyer <chadmeyer@me.com>
2012-03-21 15:16:58 -0500

    Update AppHarborAPIClientTests/Constants/AHTestsConstants.m

--- AppHarborAPIClientTests/Constants/AHTestsConstants.m
+++ AppHarborAPIClientTests/Constants/AHTestsConstants.m
@@ -1,7 +1,6 @@
 
-
 // Add your keys here for testing, you dont get to use mine!
-NSString * const kAppHarborTestClientID = @"a0b9b029-046a-404a-a378-078bc2c20aed";
-NSString * const kAppHarborTestClientSecret = @"041822e5-da82-4da6-8d27-9c1de713d37d";
-NSString * const kAppHarborTestAccessToken = @"83b82fc3-b734-4cc8-a72f-8dcbe610fcce";
+NSString * const kAppHarborTestClientID = @"";
+NSString * const kAppHarborTestClientSecret = @"";
+NSString * const kAppHarborTestAccessToken = @"";
 NSString * const kAppHarborTestRefreshToken = @"app_harbor_refresh_token";
